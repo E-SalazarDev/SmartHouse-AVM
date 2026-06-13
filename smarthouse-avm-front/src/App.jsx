30f1660465c4f276c2b0e359674f65ac260cde28
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-06-13 17:23:09 -0600

    home

--- smarthouse-avm-front/src/App.jsx
+++ smarthouse-avm-front/src/App.jsx
@@ -1,33 +1,22 @@
-import house from './assets/house.jpg'
+import Header from "./components/header";
 
 function App() {
   return (
 
-    <main className="min-h-screen bg-amber-500 flex items-center justify-center">
-      
-      <div className="w-full max-w-md rounded-2xl border border-slate-800 bg-slate-900 p-8 shadow-xl mr-7">
-        <h1 className="text-3xl font-bold text-white">
-          SmartHouse AVM
-        </h1>
+    <main className="min-h-screen bg-amber-500 flex flex-col items-center p-5 gap-6">
+        
+      <Header/>
 
-        <p className="mt-2 text-slate-400">
-          Sistema de predicción de precios de viviendas
-        </p>
 
-      
-      </div>
-
-      <div className="border-b-blue-600/95 bg-pink-500 p-10 rounded-2xl">
-
-        <img src={house} alt="Descripción de mi imagen" />
-       <div className='bg-green-500'>
-         <h1 className="text-3xl font-bold text-white text-center mt-3">
-          SmartHouse AVM
-        </h1>
+       <div className="w-full h-96 rounded-2xl border border-slate-800 bg-slate-900 p-8 shadow-xl">
+          <h1 className="text-3xl font-bold text-white">
+            Home
+          </h1>
+         
        </div>
-      </div>
+       
     </main>
   )
 }
 
-export default App
+export default App;
