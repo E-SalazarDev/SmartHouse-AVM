052c39a8a908b09b9de50959aec8418dda0220b8
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-06-13 17:22:35 -0600

    header component

---
+++ smarthouse-avm-front/src/components/header/index.jsx
@@ -0,0 +1,7 @@
+export default function Header({ }) {
+    return (
+        <div className="w-full h-20 bg-blue-600 flex p-5 rounded-xl">
+            <span className="text-white font-bold">Navbar / Barra Azul</span>
+        </div>
+    )
+}
