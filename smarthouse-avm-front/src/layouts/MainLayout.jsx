705403b95ac0bcf52177e1f0074ed50d80b79647
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-06-15 16:04:45 -0600

    create layouts: MainLayout

---
+++ smarthouse-avm-front/src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/header";
+
+export default function MainLayout() {
+  return (
+    <main className="min-h-screen p-4 md:p-5 bg-[#2e3267]">
+      <div className="flex min-h-[calc(100vh-40px)] w-full flex-col gap-6">
+        <Header />
+
+        <div className="w-full flex-1">
+          <Outlet />
+        </div>
+      </div>
+    </main>
+  );
+}
