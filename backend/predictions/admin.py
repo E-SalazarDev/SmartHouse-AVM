7834677fe78ba9a676f802c3fa2b994bb1bfdcfe
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-06-14 20:02:36 -0600

    admin predictions

--- backend/predictions/admin.py
+++ backend/predictions/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import PredictionRequest
 
-# Register your models here.
+@admin.register(PredictionRequest)
+class PredictionRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "predicted_price",
+        "model_name",
+        "model_version",
+        "created_at",
+    )
+    
+    search_fields = (
+        "model_name",
+        "model_version",
+    )
+    
+    list_filter = (
+        "model_name",
+        "created_at",
+    )
