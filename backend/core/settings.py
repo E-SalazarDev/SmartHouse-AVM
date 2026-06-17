3e69ac70c95a7100f3dc68101befd02ed0cdad96
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-06-16 20:30:26 -0600

    create properties app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'predictions',
+    'properties',
     
     'django.contrib.admin',
     'django.contrib.auth',
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/predictions/', include("predictions.urls"))
+    path('api/predictions/', include("predictions.urls")),
+    path("api/properties/", include("properties.urls")),
 ]
