096466f1ff18b607ba702a71d1cbbcc4e49b45d1
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-06-18 17:20:00 -0600

    create api post PropertyPredictionView

--- backend/properties/urls.py
+++ backend/properties/urls.py
@@ -3,18 +3,12 @@ from django.urls import path
 from .views import (
     PropertyListView,
     PropertyDetailView,
+    PropertyPredictionView
 )
 
 
 urlpatterns = [
-    path(
-        "",
-        PropertyListView.as_view(),
-        name="property-list"
-    ),
-    path(
-        "<int:property_id>/",
-        PropertyDetailView.as_view(),
-        name="property-detail"
-    ),
+    path("",PropertyListView.as_view(), name="property-list"),
+    path("<int:property_id>/", PropertyDetailView.as_view(), name="property-detail"),
+    path("<int:property_id>/predict/", PropertyPredictionView.as_view(), name="property-predict")
 ]
--- backend/properties/views.py
+++ backend/properties/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from predictions.models import PredictionRequest
+from predictions.ml_model import predict_house_price
 
 from .models import Property
 from .serializers import (
@@ -45,6 +47,51 @@ class PropertyDetailView(APIView):
             return Response(
                 serializer.data,
                 status=status.HTTP_200_OK
+            )
+
+        except Property.DoesNotExist:
+            return Response(
+                {
+                    "error": "Property not found"
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+            
+class PropertyPredictionView(APIView):
+
+    def post(self, request, property_id):
+
+        try:
+            property_obj = Property.objects.get(
+                id=property_id,
+                is_active=True
+            )
+
+            if not property_obj.model_input_data:
+                return Response(
+                    {
+                        "error": "This property does not have model input data."
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            predicted_price = predict_house_price(
+                property_obj.model_input_data
+            )
+
+            prediction = PredictionRequest.objects.create(
+                property=property_obj,
+                input_data=property_obj.model_input_data,
+                predicted_price=predicted_price
+            )
+
+            return Response(
+                {
+                    "property_id": property_obj.id,
+                    "prediction_id": prediction.id,
+                    "predicted_price": round(predicted_price, 2)
+                },
+                status=status.HTTP_201_CREATED
             )
 
         except Property.DoesNotExist:
