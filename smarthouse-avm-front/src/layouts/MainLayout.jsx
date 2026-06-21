e8ba2f1a4099dee2f0c45f4d65e26960455b1d45
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-06-20 19:36:31 -0600

    bg color main

--- smarthouse-avm-front/src/layouts/MainLayout.jsx
+++ smarthouse-avm-front/src/layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import Header from "../components/header";
 
 export default function MainLayout() {
   return (
-    <main className="min-h-screen p-4 md:p-5 bg-[#2e3267]">
+    <main className="min-h-screen p-4 md:p-5 bg-[#E6EEFA]">
       <div className="flex min-h-[calc(100vh-40px)] w-full flex-col gap-6">
         <Header />
 
