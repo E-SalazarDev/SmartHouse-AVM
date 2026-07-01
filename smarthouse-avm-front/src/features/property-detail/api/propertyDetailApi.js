950ec27d992ef4aa1ab4a09169786fc936fc13d3
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-06-30 20:28:24 -0600

    create api detail

--- smarthouse-avm-front/src/features/property-detail/api/propertyDetailApi.js
+++ smarthouse-avm-front/src/features/property-detail/api/propertyDetailApi.js
@@ -1,11 +1,11 @@
 import { apiClient } from "../../../api/apiClient";
 
-export async function getPropertiesById(propertyId) {
-    const response = await apiClient.get(`/properties/${propertyId}`)
-    return response.data
+export async function getPropertyById(propertyId) {
+    const response = await apiClient.get(`/properties/${propertyId}/`);
+    return response.data;
 }
 
 export async function postPredictPropertyPrice(propertyId) {
-    const response = await apiClient.post(`/properties/${propertyId}/predict/`)
-    return response.data
+    const response = await apiClient.post(`/properties/${propertyId}/predict/`);
+    return response.data;
 }
