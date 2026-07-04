1089b8af093105457b32a79cdd19d35068914b9b
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-07-03 19:20:48 -0600

    PropertyDescription

---
+++ smarthouse-avm-front/src/features/property-detail/components/PropertyDescription.jsx
@@ -0,0 +1,13 @@
+import SectionTitle from "./SectionTitle";
+
+export default function PropertyDescription({ description }) {
+    return (
+        <div className="p-6 pb-0">
+            <SectionTitle>Descripción</SectionTitle>
+
+            <p className="text-[14.5px] leading-7 text-slate-600">
+                {description}
+            </p>
+        </div>
+    );
+}
