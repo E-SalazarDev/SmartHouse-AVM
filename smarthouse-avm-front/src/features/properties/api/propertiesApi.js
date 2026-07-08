6c3579f3243727b783d13515a1aab729a2fefddf
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-07-07 22:34:10 -0600

    getProperties filters

--- smarthouse-avm-front/src/features/properties/api/propertiesApi.js
+++ smarthouse-avm-front/src/features/properties/api/propertiesApi.js
@@ -1,7 +1,18 @@
 import { apiClient } from "../../../api/apiClient";
 
-export async function getProperties(pageNum) {
-    const response = await apiClient.get(`/properties/?page=${pageNum}`);
+export async function getProperties(pageNum, filters = {}) {
+    const params = new URLSearchParams();
+
+    params.append("page", pageNum);
+
+    Object.entries(filters).forEach(([key, value]) => {
+        if (value !== "" && value !== null && value !== undefined) {
+            params.append(key, value);
+        }
+    });
+
+    const response = await apiClient.get(`/properties/?${params.toString()}`);
+
     return response.data;
 }
 
