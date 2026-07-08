38b01ad5df0b27389b22cbfc09ca7253b3d54305
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-07-08 16:44:50 -0600

    change pagination response

--- backend/properties/pagination.py
+++ backend/properties/pagination.py
@@ -1,6 +1,21 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 
 class PropertyPagination(PageNumberPagination):
     page_size = 12
-    page_size_query_param="page_size"
-    max_page_size=50
+    page_size_query_param = "page_size"
+    max_page_size = 50
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "count": self.page.paginator.count,
+                "page_size": self.get_page_size(self.request),
+                "current_page": self.page.number,
+                "total_pages": self.page.paginator.num_pages,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
