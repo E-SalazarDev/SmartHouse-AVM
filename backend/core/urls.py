433692c817863f3629177314e3c39f09aa945041
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-07-09 21:36:44 -0600

    url favorites api

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/predictions/', include("predictions.urls")),
     path("api/properties/", include("properties.urls")),
+    path("api/favorites/", include("favorites.urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
