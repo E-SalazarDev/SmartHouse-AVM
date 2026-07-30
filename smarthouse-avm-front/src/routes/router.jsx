5e68458d6111c0787827478352602af07bf7e3e6
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-07-29 20:00:49 -0600

    feat(routes): add protected /mi-cuenta route

--- smarthouse-avm-front/src/routes/router.jsx
+++ smarthouse-avm-front/src/routes/router.jsx
@@ -5,12 +5,13 @@ import HomePage from "../pages/Home/Home";
 import DashboardPage from "../pages/Dashboard/Dashboard";
 import PropertiesPage from "../pages/Properties/Properties";
 import PropertyDetailPage from "../pages/PropertyDetail/PropertyDetail";
-import Favorities from "../features/favorites/Favorites";
+import FavoritesPage from "../pages/Favorites/Favorites";
 import LoginPage from "../pages/Login/Login";
 import AuthProvider from "../features/auth/context/AuthProvider";
 import ProtectedRoute from "../features/auth/components/ProtectedRoute";
 import RegisterPage from "../pages/Register/Register";
 import ComparisonPage from "../pages/Comparison/Comparison";
+import AccountPage from "../pages/Account/Account";
 
 const queryClient = new QueryClient();
 
@@ -48,13 +49,21 @@ export const router = createBrowserRouter([
                 path: "favoritos",
                 element: (
                     <ProtectedRoute>
-                        <Favorities />
+                        <FavoritesPage />
                     </ProtectedRoute>
                 ),
             },
             {
                 path: "login",
                 element: <LoginPage />,
+            },
+            {
+                path: "mi-cuenta",
+                element: (
+                    <ProtectedRoute>
+                        <AccountPage />
+                    </ProtectedRoute>
+                ),
             },
             {
                 path: "registro",
