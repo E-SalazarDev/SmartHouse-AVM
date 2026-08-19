f5d84728a701eb958dc2e0094d1109aa93004801
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-08-18 22:32:34 -0600

    feat(account): restructure page into two-column layout with stats and actions

--- smarthouse-avm-front/src/features/account/Account.jsx
+++ smarthouse-avm-front/src/features/account/Account.jsx
@@ -1,6 +1,8 @@
 import useAuth from "../auth/hooks/useAuth";
 import AccountHeader from "./components/header/AccountHeader";
 import AccountInfoCard from "./components/info/AccountInfoCard";
+import AccountStats from "./components/stats/AccountStats";
+import AccountActions from "./components/actions/AccountActions";
 
 export default function Account() {
     const { user } = useAuth();
@@ -8,7 +10,17 @@ export default function Account() {
     return (
         <div className="w-full rounded-2xl border border-slate-200 bg-[#f6f7fb] p-4 md:p-6 shadow-xl flex flex-col gap-5">
             <AccountHeader />
-            <AccountInfoCard user={user} />
+
+            <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+                <div className="lg:col-span-2">
+                    <AccountInfoCard user={user} />
+                </div>
+
+                <div className="flex flex-col gap-4">
+                    <AccountStats />
+                    <AccountActions />
+                </div>
+            </div>
         </div>
     );
 }
