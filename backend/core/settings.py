9369dc92c3ab3a517ffcc6927fce4375d69ebff9
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-09-23 23:59:52 -0600

    feat: switch database to PostgreSQL

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -99,8 +99,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'smarthouse'),
+        'USER': os.environ.get('POSTGRES_USER', 'smarthouse'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'smarthouse'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
